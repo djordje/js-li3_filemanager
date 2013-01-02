8b98fed90c4c8f19c1cafb5c8dd408d79c8de7fd
djordje <djordje.bigjoe@gmail.com>
2013-01-02 15:38:40 +0100

    Updated Queue object and tests

--- src/queue.js
+++ src/queue.js
@@ -1,8 +1,29 @@
-Queue = {
+var Queue = {
+
+	/**
+	 * Last action used in `add` method
+	 * @var string
+	 */
 	lastAction: '',
+
+	/**
+	 * Array of allowed actions
+	 * @var array
+	 */
 	allowedActions: ['copy', 'move', 'remove'],
+
+	/**
+	 * Array of queued values
+	 * @var array
+	 */
 	queued: [],
 
+	/**
+	 * Add path to queue
+	 * @param string action One of allowed actions (copy|move|remove)
+	 * @param string value Path to queue
+	 * @param boolean reset Reset object before adding value to queue
+	 */
 	add: function(action, value, reset) {
 		var exists = false;
 		if (!action || !value) {
@@ -11,6 +32,7 @@ Queue = {
 		if (!!reset) {
 			this.reset();
 		}
+
 		for (var i = this.allowedActions.length - 1; i >= 0; i--) {
 			if (this.allowedActions[i] === action) {
 				this.lastAction = action;
@@ -21,26 +43,36 @@ Queue = {
 				}
 			}
 		}
+
 		if (!exists) {
 			return this.queued.push(value);
 		}
 		return false;
 	},
 
+	/**
+	 * Remove value from queue if exists
+	 * @param string value Value to search for in queue
+	 * @return boolean
+	 */
 	remove: function(value) {
-		if (this.last_action === '') {
-			return false;
-		}
-		for (var i = this.queued.length - 1; i >= 0; i--) {
-			if (this.queued[i] === value) {
-				return !this.queued.splice(i, 1);
+		if (this.lastAction !== '') {
+			var vIndex = $.inArray(value, this.queued);
+			if (vIndex >= 0) {
+				this.queued.splice(vIndex, 1);
+				return true;
 			}
 		}
+		return false;
 	},
 
+	/**
+	 * Reset object
+	 * @return void
+	 */
 	reset: function() {
 		this.lastAction = '';
-		this.actionExists = false;
 		this.queued = [];
 	}
+
 };
--- tests/unit/queue-test.js
+++ tests/unit/queue-test.js
@@ -1,5 +1,35 @@
-test('test initial state', function() {
-	equal(Queue.lastAction, '', 'There is not lastAction on initialization');
-	deepEqual(Queue.allowedActions, ['copy', 'move', 'remove'], 'Enable `copy`, `move` and `remove`');
-	deepEqual(Queue.queued, [], 'Queue should be empty on initialization');
+$(function() {
+
+	module('Queue');
+
+	test('Test initial state', function() {
+		equal(Queue.lastAction, '');
+
+		deepEqual(Queue.allowedActions, ['copy', 'move', 'remove']);
+
+		deepEqual(Queue.queued, []);
+	});
+
+	test('Test object methods', function() {
+		equal(!!Queue.add('copy', 'path'), true);
+
+		equal(!!Queue.add('copy', 'path2'), true);
+
+		equal(Queue.add('copy', 'path'), false);
+
+		equal(Queue.remove('path3'), false);
+
+		equal(Queue.remove('path2'), true);
+
+		equal(!!Queue.add('copy', 'path', true), true);
+	});
+
+	test('Test reset method and returning object to initial state', function() {
+		Queue.reset();
+
+		equal(Queue.lastAction, '');
+
+		deepEqual(Queue.queued, []);
+	});
+
 });
